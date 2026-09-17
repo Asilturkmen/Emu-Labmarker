08da09bc17968a04cd924b9c4df1dfaf5bd56303
Asil <Turkmenasil@hotmail.com>
2026-09-17 18:34:41 +0300

    icon eklendi

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
     name: "EMU LabMark",
     description: "Highlights verified and probable laboratory rooms in the EMU timetable.",
     version: "0.1.0",
+    icons: {
+      16: "icons/icon-16.png",
+      32: "icons/icon-32.png",
+      48: "icons/icon-48.png",
+      128: "icons/icon-128.png",
+    },
   },
 });
